57c14a64906bdafd8b488e6cee4cc264b8d61967
Jeremy McDuffie <jmmcduffie@users.noreply.github.com>
2020-04-24 21:38:26 -0500

    Add blog posts to home page

--- src/components/masthead.js
+++ src/components/masthead.js
@@ -1,10 +1,27 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import { useStaticQuery, Link, graphql } from 'gatsby'
 
-export default () => (
-  <header role="banner" className="border-b py-4 px-4 text-gray-700">
-    <div className="max-w-screen-lg mx-auto">
-      <Link to="/" rel="author" className="text-2xl hover:text-blue-700">jmmcduffie.com</Link>
-    </div>
-  </header>
-)
+
+export default () => {
+  const data = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `
+  )
+
+  return (
+    <header role="banner" className="border-b py-4 px-4 text-gray-700">
+      <div className="max-w-screen-lg mx-auto">
+        <Link to="/" rel="author" className="text-2xl hover:text-blue-700">
+          {data.site.siteMetadata.title}
+        </Link>
+      </div>
+    </header>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,34 @@
 import React from 'react'
+import { graphql } from "gatsby"
 import Layout from '../components/layout'
 
-export default () => (
+export default ({ data }) => (
   <Layout>
-    Hello world!
+    <h2 className="text-lg font-bold mb-4">Blog</h2>
+
+    <ol className="space-y-6">
+      {data.allMarkdownRemark.edges.map(({ node }) => (
+        <li key={node.id}>
+          <span className="text-gray-600 text-sm">{node.frontmatter.date}</span>
+          <h3 className="text-2xl leading-tight">{node.frontmatter.title}</h3>
+        </li>
+      ))}
+    </ol>
   </Layout>
 )
+
+export const query = graphql`
+  query {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "MMMM DD, YYYY")
+          }
+        }
+      }
+    }
+  }
+`

7de0828ce40a0a59adfd25cc2e964e325eaa2f27
Jeremy McDuffie <jmmcduffie@users.noreply.github.com>
2020-04-29 12:33:33 -0500

    Add 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link, graphql } from "gatsby"
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+
+export default () => (
+  <Layout>
+    <SEO title="Page Not Found" meta={{name: 'robots', content: 'noindex'}} />
+    <h1 className="text-3xl md:text-4xl leading-tight mb-6">Page Not Found</h1>
+    <p className="mb-2">It looks like you found a broken link. Sorry!</p>
+    <p><Link to="/" className="text-blue-500 hover:underline">&larr; Back to Home Page</Link></p>
+  </Layout>
+)
